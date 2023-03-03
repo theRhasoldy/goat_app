fa75cc6597ac257a181a3ff0b85216265c15494f
Nour El Din Shady <rhasoldy@gmail.com>
2023-03-03 18:54:36 +0200

    Added BottomNavBar widget

--- lib/SignUp.dart
+++ lib/SignUp.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:goat_app/common/config/theme.dart';
+import 'package:goat_app/common/widgets/bottom_navigation_bar.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -12,16 +13,14 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme:mainTheme,
+      theme: mainTheme,
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-
         appBar: AppBar(
           title: Text('G.O.A.T'),
-
         ),
+        bottomNavigationBar: BottomNavBar(),
       ),
     );
   }
 }
-
---
+++ lib/common/widgets/bottom_navigation_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.tv), label: "TV")
+      ],
+    );
+  }
+}
