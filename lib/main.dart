3bfb62255e0956ea1a6c7a18d22b37e61bfd166b
Nour El Din Shady <rhasoldy@gmail.com>
2023-03-03 18:17:18 +0200

    Fixed main

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:goat_app/SignUp.dart';
+
 void main() {
   runApp(GOAT());
 }
+
 class GOAT extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Provider(
-        create: (BuildContext context) {  },
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData.dark().copyWith(
-            primaryColor: Color(0xFF0A0E21),
-            scaffoldBackgroundColor: Color(0xFF0A0E21),
-          ),
-          home: SignUp(),
-        ));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignUp(),
+    );
   }
 }
