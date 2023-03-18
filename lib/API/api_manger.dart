f9e6fdd11871b50828031147c3a835d4c0375cf7
Nour El Din Shady <rhasoldy@gmail.com>
2023-03-18 04:01:01 +0200

    Specify id to get a venue

--- lib/API/api_manger.dart
+++ lib/API/api_manger.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 
 class APIService {
   static final dio = Dio();
-  static final url = 'https://v3.football.api-sports.io/fixtures?live=all';
+  static final id = "133";
+  static final url = 'https://v3.football.api-sports.io/venues?id=' + id;
   static final apiKey = 'cad2a28d9c7384611d1aaaf4643623a2';
   // replace with your API key
 
--- lib/models/soccer.dart
+++ lib/models/soccer.dart
@@ -11,9 +11,4 @@ class Team {
         id: json["id"],
         name: json["name"],
       );
-
-  Map<String, dynamic> toJson() => {
-        "id": id,
-        "name": name,
-      };
 }
