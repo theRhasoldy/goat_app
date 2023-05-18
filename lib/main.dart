61d9852c2a19f806eb8c3e67ef14545aa43a52a7
ShadyMagdy20192186 <20192186@fue.edu.eg>
2023-05-18 17:30:46 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: screen(),
+      home: SignUp(),
       theme: mainTheme,
     );
   }
